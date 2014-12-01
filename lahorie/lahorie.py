9da90abbe48a69c78353c02819830187d03f76fa
salas106 <salas.106.212@gmail.com>
2014-12-02 00:26:18 +0100

    Boostraping lahorie

---
+++ lahorie/lahorie.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+# -*- coding: utf8 -*-
+
+"""
+    The ``main`` module
+    ===================
+
+    Just start it and it will read the configuration file (if existing) or create one step by step (if not).
+"""
+
+__author__ = 'Salas'
+__copyright__ = 'Copyright 2014 LTL'
+__credits__ = ['Salas']
+__license__ = 'MIT'
+__version__ = '0.2.0'
+__maintainer__ = 'Salas'
+__email__ = 'Salas.106.212@gmail.com'
+__status__ = 'Pre-Alpha'
+
+
